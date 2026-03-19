d869dd9ec8c36068dfec90e873598cdf19ff6e31
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2026-03-19 05:31:23 +0100

    Update Sysinfo.js

--- Sysinfo.js
+++ Sysinfo.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Sysinfo Plugin',
-    version: '1.3',
+    version: '1.3a',
     author: 'Highpoint',
     frontEndPath: 'Sysinfo/sysinfo.js'
 }
